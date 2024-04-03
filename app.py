547f389a73d20b5c5734d2c30235ed622bbc4c9e
Rob Smith <robsmith@binaryrage.com>
2024-04-03 10:55:06 +0100

    Initial commit, basic hello-world app and .gitignore

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+app.py
+
+An extraordinarily simple 'hello world' application which uses flask to 
+create a simple /hello-world end-point. This project exists largely to 
+show an example pipeline for deploying code to a GCP Cloud-Run instance. 
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello-world")
+def hello_world():
+    return "<p>Hello, World!</p>"
+ 
