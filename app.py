19aaad3674398df3c5b9201f730c215c43990ce2
Rob Smith <robsmith@binaryrage.com>
2024-06-13 13:06:07 +0100

    Merged some additional improvements, such as including Google Secrets manager

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ create a simple /hello-world end-point. This project exists largely to
 show an example pipeline for deploying code to a GCP Cloud-Run instance. 
 
 """
+import os
+import json
+import logging
+from google.cloud import secretmanager
+
+
+# Initalise the connection to Secrets Manager
+secrets_client = secretmanager.SecretManagerServiceClient()
+
+# Pull all of the relevant secrets, we get the reference for the secret with the environment variable SECRET_REF
+SECRET_REF          = os.environ['SECRET_REF']
+secret_data_raw     = secrets_client.access_secret_version(request={"name": SECRET_REF})
+secret_decoded      = secret_data_raw.payload.data.decode("UTF-8")
+secrets_data        = json.loads(secret_decoded)
+
 
 from flask import Flask
 
